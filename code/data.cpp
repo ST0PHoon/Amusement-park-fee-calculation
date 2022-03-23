ae24e5f5211877d969d714ddca43216bde181425
ST0PHoon <codingnb306@naver.com>
2022-03-23 15:16:51 +0900

    날짜별 분석

--- code/data.cpp
+++ code/data.cpp
@@ -7,7 +7,8 @@ int main () {
 	int count = 0;
 	int year[MAX], month[MAX], day[MAX],
 		ticketTimeCaseArr[MAX],wooDaeArr[MAX],ageCaseArr[MAX],ticketSuArr[MAX],ticketPriceArr[MAX];
-	int wooDae1=0,wooDae2=0,wooDae3=0,wooDae4=0,wooDae5=0,wooDae6=0;
+	int wooDae1=0,wooDae2=0,wooDae3=0,wooDae4=0,wooDae5=0,wooDae6=0,
+		k, dayTotal=0;
 		
 	while ( fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d",&year[count], &month[count], &day[count],
 		&ticketTimeCaseArr[count], &wooDaeArr[count], &ageCaseArr[count], &ticketSuArr[count], &ticketPriceArr[count]) != -1) {
@@ -19,10 +20,28 @@ int main () {
 	//year[i], month[i], day[i], ticketTimeCaseArr[i], wooDaeArr[i], ageCaseArr[i], ticketSuArr[i], ticketPriceArr[i]
 	for(int i = 0; i < count; i++) {
 	//일자 별 총 매출 
-		// 날짜를 담을 행렬과 해당 날짜의 총액을 담을 행렬을 만든다.
-		// for 문을 돌리면서 날짜가 동일할 경우 가격을 누적한다.
-		// 날짜가 동일하지 않을 경우, 행렬의 다음칸에 새로운 날짜와 가격을 입력한다.
-		// 마지막에 분석한 데이터 메세지로 출력 
+		// 날짜가 별로 내림차순 되어있다고 가정
+		k= i-1;
+		if (k == -1) {
+			k = 0;
+		}
+		//마지막 날과 전날이 같은 경우 > 값을 더하고 출력  
+		//마지막 날과 전날이 다른 경우 > 오늘의 티켓 가격을 출력 
+		
+		if (year[i]==year[k]&& month[i]==month[k]&& day[i]==day[k]) {
+			dayTotal += ticketPriceArr[i];
+			if (i==count-1) {
+				printf("%d-%d-%d, %d\n",year[k],month[k],day[k], dayTotal);
+			} 
+		} else {
+			if (i==count-1) {
+				printf("%d-%d-%d, %d\n",year[i],month[i],day[i], ticketPriceArr[i]);
+			} else {
+				printf("%d-%d-%d, %d\n",year[k],month[k],day[k], dayTotal);
+				dayTotal=0;
+				dayTotal +=ticketPriceArr[i];
+			}
+		}
 	
 	//티켓 종류 별, 연령대 구매 분석 / 시간대 별 매출액
 		//연령에 따른 종합, 파크 and 1Day, After4 담을 행렬을 만든다. (연령6가지 * 티켓 4가지 = 24개의 변수)
