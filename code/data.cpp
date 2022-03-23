bf93311026f5f2bb23d208b0aaf76a8c13dee00b
ST0PHoon <codingnb306@naver.com>
2022-03-23 14:25:45 +0900

    데이터 추가, 우대사항 별 발권 수

--- code/data.cpp
+++ code/data.cpp
@@ -7,6 +7,7 @@ int main () {
 	int count = 0;
 	int year[MAX], month[MAX], day[MAX],
 		ticketTimeCaseArr[MAX],wooDaeArr[MAX],ageCaseArr[MAX],ticketSuArr[MAX],ticketPriceArr[MAX];
+	int wooDae1=0,wooDae2=0,wooDae3=0,wooDae4=0,wooDae5=0,wooDae6=0;
 		
 	while ( fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d",&year[count], &month[count], &day[count],
 		&ticketTimeCaseArr[count], &wooDaeArr[count], &ageCaseArr[count], &ticketSuArr[count], &ticketPriceArr[count]) != -1) {
@@ -15,11 +16,58 @@ int main () {
 	}
 	fclose(fp);
 	
-	// 데이터 분석할 코드 구간 
+	//year[i], month[i], day[i], ticketTimeCaseArr[i], wooDaeArr[i], ageCaseArr[i], ticketSuArr[i], ticketPriceArr[i]
 	for(int i = 0; i < count; i++) {
-		printf("%d,%d,%d,%d,%d,%d,%d,%d\n",year[i], month[i], day[i],
-		ticketTimeCaseArr[i], wooDaeArr[i], ageCaseArr[i], ticketSuArr[i], ticketPriceArr[i] );
+	//일자 별 총 매출 
+		// 날짜를 담을 행렬과 해당 날짜의 총액을 담을 행렬을 만든다.
+		// for 문을 돌리면서 날짜가 동일할 경우 가격을 누적한다.
+		// 날짜가 동일하지 않을 경우, 행렬의 다음칸에 새로운 날짜와 가격을 입력한다.
+		// 마지막에 분석한 데이터 메세지로 출력 
+	
+	//티켓 종류 별, 연령대 구매 분석 / 시간대 별 매출액
+		//연령에 따른 종합, 파크 and 1Day, After4 담을 행렬을 만든다. (연령6가지 * 티켓 4가지 = 24개의 변수)
+		//돌리면서 변수에 값을 추가, 분석해서 값을 내보낸다. 
+	
+	//총 티켓 판매 수, 혜택별 티켓수 분석
+		//마지막 인원수는 count 값
+		//혜택에 따라 티켓을 나눈다. 
+			switch (wooDaeArr[i]) {
+				case 1:
+					wooDae1++;
+					break;
+				case 2:
+					wooDae2++;
+					break;
+				case 3:
+					wooDae3++;
+					break;
+				case 4:
+					wooDae4++;
+					break;
+				case 5:
+					wooDae5++;
+					break;
+				case 6:
+					wooDae6++;
+					break;
+				default:
+					break;
+			}
 	}
+	
+	// 메시지 출력구간
+	//일자 별 총 매출
+	
+	//티켓 종류 별, 연령대 구매 분석 / 시간대 별 매출액
+	
+	//총 티켓 판매 수, 혜택별 티켓수 분석
+	printf("총 티켓 판매수: %d\n",count);
+	printf("우대사항 없음: %d\n",wooDae1);
+	printf("장애인 우대: %d\n",wooDae2);
+	printf("국가유공자 우대: %d\n",wooDae3);
+	printf("휴가장병 우대: %d\n",wooDae4);
+	printf("임산부 우대: %d\n",wooDae5);
+	printf("다둥이 행복카드 우대: %d\n",wooDae6);
 	
 	return 0;
 }
