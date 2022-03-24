9b54eb9d016f44ffe5c01d2c8300ee39f0ba8c67
ST0PHoon <codingnb306@naver.com>
2022-03-24 09:32:09 +0900

    우대사항, 티켓_나이별 분류, 데이터 순서 조정 및 출력방식 변경

--- code/data.cpp
+++ code/data.cpp
@@ -7,10 +7,10 @@ int main () {
 	int count = 0, cir=0;
 	int year[MAX], month[MAX], day[MAX],
 		ticketTimeCaseArr[MAX],wooDaeArr[MAX],ageCaseArr[MAX],ticketSuArr[MAX],ticketPriceArr[MAX],
-		ageTicketCase[MAX][MAX]={0};
+		ageTicketCase[MAX][MAX]={0}, wooDeaArrOut[30]={0};
 	int wooDae1=0,wooDae2=0,wooDae3=0,wooDae4=0,wooDae5=0,wooDae6=0,
 		parkMoney=0, parkAllMoney=0, oneDayMoney=0, After4Money=0,
-		k, dayTotal=0, totalTicketSu=0,
+		k, dayTotal=0, 
 		parkTicketSu,parkAllTicketSu,oneDayTicketSu, After4TicketSu;
 		
 	while ( fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d",&year[count], &month[count], &day[count],
@@ -36,130 +36,138 @@ int main () {
 			case 1: //종합 + 1Day 
 				switch(ageCaseArr[i]) {
 					case 1: //어른
-						ageTicketCase[0][0] += ticketSuArr[i];
-						ageTicketCase[2][0] += ticketSuArr[i];
-						break;
- 					case 2: //청소년 
 						ageTicketCase[0][1] += ticketSuArr[i];
 						ageTicketCase[2][1] += ticketSuArr[i];
-						break;	
-					case 3: //어린이 
+						break;
+ 					case 2: //청소년 
 						ageTicketCase[0][2] += ticketSuArr[i];
 						ageTicketCase[2][2] += ticketSuArr[i];
-						break;
-					case 4: //유아 
+						break;	
+					case 3: //어린이 
 						ageTicketCase[0][3] += ticketSuArr[i];
 						ageTicketCase[2][3] += ticketSuArr[i];
 						break;
-					case 5: //노인 
+					case 4: //유아 
 						ageTicketCase[0][4] += ticketSuArr[i];
 						ageTicketCase[2][4] += ticketSuArr[i];
 						break;
-					case 6: //영아  
+					case 5: //노인 
 						ageTicketCase[0][5] += ticketSuArr[i];
 						ageTicketCase[2][5] += ticketSuArr[i];
+						break;
+					case 6: //영아  
+						ageTicketCase[0][6] += ticketSuArr[i];
+						ageTicketCase[2][6] += ticketSuArr[i];
 						break;
 					default:
 						break; 
 				}
-				parkAllMoney += ticketPriceArr[i];
-				oneDayMoney += ticketPriceArr[i];
+				ageTicketCase[0][7] += ticketPriceArr[i];  //티켓 종류별 가격 
+				ageTicketCase[2][7] += ticketPriceArr[i];
+				ageTicketCase[0][0] += ticketSuArr[i];     // 티켓 종류별 매수 
+				ageTicketCase[2][0] += ticketSuArr[i];
 				break;
 			case 2: //종합 + 4시후 
 				switch(ageCaseArr[i]) {
 					case 1: //어른
-						ageTicketCase[0][0] += ticketSuArr[i];
-						ageTicketCase[3][0] += ticketSuArr[i];
-						break;
- 					case 2: //청소년 
 						ageTicketCase[0][1] += ticketSuArr[i];
 						ageTicketCase[3][1] += ticketSuArr[i];
-						break;	
-					case 3: //어린이 
+						break;
+ 					case 2: //청소년 
 						ageTicketCase[0][2] += ticketSuArr[i];
 						ageTicketCase[3][2] += ticketSuArr[i];
-						break;
-					case 4: //유아 
+						break;	
+					case 3: //어린이 
 						ageTicketCase[0][3] += ticketSuArr[i];
 						ageTicketCase[3][3] += ticketSuArr[i];
 						break;
-					case 5: //노인 
+					case 4: //유아 
 						ageTicketCase[0][4] += ticketSuArr[i];
 						ageTicketCase[3][4] += ticketSuArr[i];
 						break;
-					case 6: //영아  
+					case 5: //노인 
 						ageTicketCase[0][5] += ticketSuArr[i];
 						ageTicketCase[3][5] += ticketSuArr[i];
+						break;
+					case 6: //영아  
+						ageTicketCase[0][6] += ticketSuArr[i];
+						ageTicketCase[3][6] += ticketSuArr[i];
 						break;
 					default:
 						break; 
 				}
-				parkAllMoney += ticketPriceArr[i];
-				After4Money += ticketPriceArr[i];
+				ageTicketCase[0][7] += ticketPriceArr[i];
+				ageTicketCase[3][7] += ticketPriceArr[i];
+				ageTicketCase[0][0] += ticketSuArr[i];
+				ageTicketCase[3][0] += ticketSuArr[i];
 				break;
 			case 3: //파크 + 1Day
 				switch(ageCaseArr[i]) {
 					case 1: //어른
-						ageTicketCase[1][0] += ticketSuArr[i];
-						ageTicketCase[2][0] += ticketSuArr[i];
-						break;
- 					case 2: //청소년 
 						ageTicketCase[1][1] += ticketSuArr[i];
 						ageTicketCase[2][1] += ticketSuArr[i];
-						break;	
-					case 3: //어린이 
+						break;
+ 					case 2: //청소년 
 						ageTicketCase[1][2] += ticketSuArr[i];
 						ageTicketCase[2][2] += ticketSuArr[i];
-						break;
-					case 4: //유아 
+						break;	
+					case 3: //어린이 
 						ageTicketCase[1][3] += ticketSuArr[i];
 						ageTicketCase[2][3] += ticketSuArr[i];
 						break;
-					case 5: //노인 
+					case 4: //유아 
 						ageTicketCase[1][4] += ticketSuArr[i];
 						ageTicketCase[2][4] += ticketSuArr[i];
 						break;
-					case 6: //영아  
+					case 5: //노인 
 						ageTicketCase[1][5] += ticketSuArr[i];
 						ageTicketCase[2][5] += ticketSuArr[i];
+						break;
+					case 6: //영아  
+						ageTicketCase[1][6] += ticketSuArr[i];
+						ageTicketCase[2][6] += ticketSuArr[i];
 						break;
 					default:
 						break; 
 				}
-				parkMoney += ticketPriceArr[i];
-				oneDayMoney += ticketPriceArr[i];
+				ageTicketCase[1][7] += ticketPriceArr[i];
+				ageTicketCase[2][7] += ticketPriceArr[i];
+				ageTicketCase[1][0] += ticketSuArr[i];
+				ageTicketCase[2][0] += ticketSuArr[i];
 				break;
 			case 4: //파크 + 4시후
 				switch(ageCaseArr[i]) {
 					case 1: //어른
-						ageTicketCase[1][0] += ticketSuArr[i];
-						ageTicketCase[3][0] += ticketSuArr[i];
-						break;
- 					case 2: //청소년 
 						ageTicketCase[1][1] += ticketSuArr[i];
 						ageTicketCase[3][1] += ticketSuArr[i];
-						break;	
-					case 3: //어린이 
+						break;
+ 					case 2: //청소년 
 						ageTicketCase[1][2] += ticketSuArr[i];
 						ageTicketCase[3][2] += ticketSuArr[i];
-						break;
-					case 4: //유아 
+						break;	
+					case 3: //어린이 
 						ageTicketCase[1][3] += ticketSuArr[i];
 						ageTicketCase[3][3] += ticketSuArr[i];
 						break;
-					case 5: //노인 
+					case 4: //유아 
 						ageTicketCase[1][4] += ticketSuArr[i];
 						ageTicketCase[3][4] += ticketSuArr[i];
 						break;
-					case 6: //영아  
+					case 5: //노인 
 						ageTicketCase[1][5] += ticketSuArr[i];
 						ageTicketCase[3][5] += ticketSuArr[i];
+						break;
+					case 6: //영아  
+						ageTicketCase[1][6] += ticketSuArr[i];
+						ageTicketCase[3][6] += ticketSuArr[i];
 						break;
 					default:
 						break; 
 				}
-				parkMoney += ticketPriceArr[i];
-				After4Money += ticketPriceArr[i];
+				ageTicketCase[1][7] += ticketPriceArr[i];
+				ageTicketCase[3][7] += ticketPriceArr[i];
+				ageTicketCase[1][0] += ticketSuArr[i];
+				ageTicketCase[3][0] += ticketSuArr[i];
 				break;
 			default:
 				break;
@@ -170,86 +178,80 @@ int main () {
 		//혜택에 따라 티켓을 나눈다. 
 			switch (wooDaeArr[i]) {
 				case 1:
-					wooDae1 += ticketSuArr[i];
+					wooDeaArrOut[1] += ticketSuArr[i];
 					break;
 				case 2:
-					wooDae2 += ticketSuArr[i];
+					wooDeaArrOut[2] += ticketSuArr[i];
 					break;
 				case 3:
-					wooDae3 += ticketSuArr[i];
+					wooDeaArrOut[3] += ticketSuArr[i];
 					break;
 				case 4:
-					wooDae4 += ticketSuArr[i];
+					wooDeaArrOut[4] += ticketSuArr[i];
 					break;
 				case 5:
-					wooDae5 += ticketSuArr[i];
+					wooDeaArrOut[5] += ticketSuArr[i];
 					break;
 				case 6:
-					wooDae6 += ticketSuArr[i];
+					wooDeaArrOut[6] += ticketSuArr[i];
 					break;
 				default:
 					break;
 			}
 	}
-	
-	// 권종 별 총 티켓 수
-	parkTicketSu = ageTicketCase[1][5]+ageTicketCase[1][3]+ageTicketCase[1][2]+ageTicketCase[1][1]+ageTicketCase[1][0]+ageTicketCase[1][4];
-	parkAllTicketSu = ageTicketCase[0][5]+ageTicketCase[0][3]+ageTicketCase[0][2]+ageTicketCase[0][1]+ageTicketCase[0][0]+ageTicketCase[0][4];
-	oneDayTicketSu = ageTicketCase[2][5]+ageTicketCase[2][3]+ageTicketCase[2][2]+ageTicketCase[2][1]+ageTicketCase[2][0]+ageTicketCase[2][4];
-	After4TicketSu =  ageTicketCase[3][5]+ageTicketCase[3][3]+ageTicketCase[3][2]+ageTicketCase[3][1]+ageTicketCase[3][0]+ageTicketCase[3][4];
+
 	//총 티켓 수
-	totalTicketSu = wooDae1+wooDae2+wooDae3+wooDae4+wooDae5+wooDae6;
+	for (int i =1; i<=6; i++) {
+		wooDeaArrOut[0]+=wooDeaArrOut[i];
+	} 
 	
 	//파일로 출력, 아래 fpirntf 로 값 저장
 	FILE *filePointer1, *filePointer2,*filePointer3;
-	filePointer1=fopen("ticketdata.csv","a");
-	filePointer2=fopen("woodaedata.csv","a");
-	filePointer3=fopen("daydata.csv","a");
+	filePointer1=fopen("ticketdata.csv","w");
+	filePointer2=fopen("woodaedata.csv","w");
+	filePointer3=fopen("daydata.csv","w");
 
 	//임시, 이후 for 문을 이용하여 수정할 예정
- 	fprintf(filePointer1,"%d,%d,%d,%d,%d\n%d,%d,%d,%d,%d\n%d,%d,%d,%d,%d\n%d,%d,%d,%d,%d\n%d,%d,%d,%d,%d\n%d,%d,%d,%d,%d\n%d,%d,%d,%d,%d\n",
-		1,ageTicketCase[0][0],ageTicketCase[1][0],ageTicketCase[2][0],ageTicketCase[3][0],
-		2,ageTicketCase[0][1],ageTicketCase[1][1],ageTicketCase[2][1],ageTicketCase[3][1],
-		3,ageTicketCase[0][2],ageTicketCase[1][2],ageTicketCase[2][2],ageTicketCase[3][2],
-		4,ageTicketCase[0][3],ageTicketCase[1][3],ageTicketCase[2][3],ageTicketCase[3][3],
-		5,ageTicketCase[0][4],ageTicketCase[1][4],ageTicketCase[2][4],ageTicketCase[3][4],
-		6,ageTicketCase[0][5],ageTicketCase[1][5],ageTicketCase[2][5],ageTicketCase[3][5],
-		7,parkAllMoney,parkMoney,oneDayMoney,After4Money);
-	fprintf(filePointer2,"%d,%d\n%d,%d\n%d,%d\n%d,%d\n%d,%d\n%d,%d\n%d,%d\n",0,totalTicketSu,1,wooDae1,2,wooDae2,3,wooDae3,4,wooDae4,5,wooDae5,6,wooDae6);
-
+	for ( int i = 0; i <=7;i++) {
+		fprintf(filePointer1,"%d,%d,%d,%d,%d\n", i,ageTicketCase[0][i],ageTicketCase[1][i],ageTicketCase[2][i],ageTicketCase[3][i]);
+	}
+	//우대 데이터 출력 
+	for (int i = 0; i<=6;i++) {
+		fprintf(filePointer2,"%d,%d\n",i,wooDeaArrOut[i]);
+	}
 
 	//티켓 종류 별, 연령대 구매 분석 / 시간대 별 매출액 
 	printf("\n======================= 권종 별 판매 현황 =======================\n");
-	printf("종합권 총 %d매\n",parkAllTicketSu);
+	printf("종합권 총 %d매\n",ageTicketCase[0][0]);
 	printf("영아 %d매,유아 %d매, 어린이 %d매, 청소년 %d매, 어른 %d매, 노인 %d매\n",ageTicketCase[0][5],
 						ageTicketCase[0][3],ageTicketCase[0][2],ageTicketCase[0][1],ageTicketCase[0][0],ageTicketCase[0][4]);
-	printf("종합권 매출 : %d원\n\n",parkAllMoney);
+	printf("종합권 매출 : %d원\n\n",ageTicketCase[0][7]);
 	
-	printf("파크권 총 %d매\n",parkTicketSu);
+	printf("파크권 총 %d매\n",ageTicketCase[1][0]);
 	printf("영아 %d매,유아 %d매, 어린이 %d매, 청소년 %d매, 어른 %d매, 노인 %d매\n",ageTicketCase[1][5],
 						ageTicketCase[1][3],ageTicketCase[1][2],ageTicketCase[1][1],ageTicketCase[1][0],ageTicketCase[1][4]);
-	printf("파크권 매출 : %d원\n\n",parkMoney); 
+	printf("파크권 매출 : %d원\n\n",ageTicketCase[1][7]); 
 
-	printf("1Day권 총 %d매\n",oneDayTicketSu);
+	printf("1Day권 총 %d매\n",ageTicketCase[2][0]);
 	printf("영아 %d매,유아 %d매, 어린이 %d매, 청소년 %d매, 어른 %d매, 노인 %d매\n",ageTicketCase[2][5],
 						ageTicketCase[2][3],ageTicketCase[2][2],ageTicketCase[2][1],ageTicketCase[2][0],ageTicketCase[2][4]);
-	printf("1Day권 매출 : %d원\n\n",oneDayMoney); 
+	printf("1Day권 매출 : %d원\n\n",ageTicketCase[2][7]); 
 
-	printf("4시권 총 %d매\n",After4TicketSu);
+	printf("4시권 총 %d매\n",ageTicketCase[3][0]);
 	printf("영아 %d매,유아 %d매, 어린이 %d매, 청소년 %d매, 어른 %d매, 노인 %d매\n",ageTicketCase[3][5],
 						ageTicketCase[3][3],ageTicketCase[3][2],ageTicketCase[3][1],ageTicketCase[3][0],ageTicketCase[3][4]);
-	printf("4시권 매출 : %d원\n",After4Money); 
+	printf("4시권 매출 : %d원\n",ageTicketCase[3][7]); 
 	printf("================================================================\n"); 
 	
 	//총 티켓 판매 수, 혜택별 티켓수 분석
 	printf("\n=================== 우대사항 분석 ===================\n");
-	printf("총 티켓 판매수       : %d매\n",totalTicketSu);
-	printf("우대사항 없음        : %d매\n",wooDae1);
-	printf("장애인 우대          : %d매\n",wooDae2);
-	printf("국가유공자 우대      : %d매\n",wooDae3);
-	printf("휴가장병 우대        : %d매\n",wooDae4);
-	printf("임산부 우대          : %d매\n",wooDae5);
-	printf("다둥이 행복카드 우대 : %d매\n",wooDae6);
+	printf("총 티켓 판매수       : %d매\n",wooDeaArrOut[0]);
+	printf("우대사항 없음        : %d매\n",wooDeaArrOut[1]);
+	printf("장애인 우대          : %d매\n",wooDeaArrOut[2]);
+	printf("국가유공자 우대      : %d매\n",wooDeaArrOut[3]);
+	printf("휴가장병 우대        : %d매\n",wooDeaArrOut[4]);
+	printf("임산부 우대          : %d매\n",wooDeaArrOut[5]);
+	printf("다둥이 행복카드 우대 : %d매\n",wooDeaArrOut[6]);
 	printf("======================================================\n");
 	
 	//일자 별 총 매출 
@@ -289,4 +291,3 @@ int main () {
 	
 	return 0;
 }
-
